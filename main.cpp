e25c2a694e1f546a1af742feab9be2fc6b1ebee3
Pizarman13 <146822082+Pizarman13@users.noreply.github.com>
2023-11-23 11:23:38 +0000

    ej3

--- main.cpp
+++ main.cpp
@@ -10,6 +10,12 @@ int main() {
     std::cout << "Tamaño de la cadena: " << sentence.size() << std::endl;
     std::cout << "¿Está la cadena vacía? " << (sentence.empty() ? "Sí" : "No") << std::endl;
     std::cout << "Caracter en la posición 0: " << sentence.at(0) << std::endl;
+
+    std::string str1 = "x";
+    std::string str2 = "d";
+    std::string str3 = " = no se q contestar";
+    std::cout << "Cadena resultante: " << str1 + str2 +str3 << std::endl;
+
     
     
     return 0;
