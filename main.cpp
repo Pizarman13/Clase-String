3a35dcb92b7084f403e9512abde95edbff910dc4
Pizarman13 <146822082+Pizarman13@users.noreply.github.com>
2023-11-23 11:16:43 +0000

    ej1

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+    std::string texto = "Hello, World!, pero ahora en una string";
+    std::cout << texto << std::endl;
+
+    
+    return 0;
+}
