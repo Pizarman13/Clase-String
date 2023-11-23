13b258d432e52f78f109a662bba32532fedc596b
Pizarman13 <146822082+Pizarman13@users.noreply.github.com>
2023-11-23 11:20:01 +0000

    ej2

--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@ int main() {
     std::string texto = "Hello, World!, pero ahora en una string";
     std::cout << texto << std::endl;
 
+    std::string sentence = "Ataque a los titanes esta sobrebalorado";
+    std::cout << "Tamaño de la cadena: " << sentence.size() << std::endl;
+    std::cout << "¿Está la cadena vacía? " << (sentence.empty() ? "Sí" : "No") << std::endl;
+    std::cout << "Caracter en la posición 0: " << sentence.at(0) << std::endl;
+    
     
     return 0;
 }
