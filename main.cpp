7c052d26f1af4e696c3aadaba3409afac735a270
Pizarman13 <146822082+Pizarman13@users.noreply.github.com>
2023-11-23 11:27:39 +0000

    ej4

--- main.cpp
+++ main.cpp
@@ -16,7 +16,19 @@ int main() {
     std::string str3 = " = no se q contestar";
     std::cout << "Cadena resultante: " << str1 + str2 +str3 << std::endl;
 
+    std::string frase = "El lenguaje C++ es poderoso.";
+    std::string sub_str = "C++";
     
+    size_t pos = frase.find(sub_str);
+    
+    if(pos != std::string::npos)
+        std::cout << "Subcadena encontrada en la posición: " << pos << std::endl;
+    else
+        std::cout << "Subcadena no encontrada." << std::endl;
+    
+    std::string new_sentence = sentence.replace(pos, sub_str.length(), "Python");
+    
+    std::cout << "Cadena resultante: " << new_sentence << std::endl;
     
     return 0;
 }
